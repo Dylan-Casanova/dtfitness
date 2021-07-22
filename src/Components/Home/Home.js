d1a32675ae10a9c65dfb80515a22b1a2cea3bcba
Dylan Casanova <dyylancasanova@hotmail.com>
2021-07-22 17:38:17 -0500

    implemented form, connecting buttons to js next.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import './Home.css';
 
 class Home extends Component {
@@ -7,6 +10,30 @@ class Home extends Component {
       
         <div class="container welcome-message">
         <h1 className="text-center"> Welcome to DTFitness!</h1>
+        <div id='form'>
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="email" placeholder="Enter Username" />
+    <Form.Text className="text-muted">
+      Don't have an account? Find the SignUp button!
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Ready for the pump" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+
+        </div>
       </div>
       
       
